3e63db79032c1daafb15aed7994d0ab4fd07b571
mochammadfawwaz <mailmcfawwaz@gmail.com>
2021-09-02 09:53:19 +0700

    pekerjaan fawwaz

---
+++ app/src/main/java/com/fs/monize/ui/activity/menabung/Menabung.kt
@@ -0,0 +1,11 @@
+package com.fs.monize.ui.activity.menabung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menabung : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menabung)
+    }
+}
