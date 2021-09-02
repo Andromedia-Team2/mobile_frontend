2c4c6c4900f1932315e026c418063e58800f5c61
ianfyan <imamkurfian@gmail.com>
2021-09-02 09:37:30 +0700

    round_bg

--- app/src/main/java/com/fs/monize/ui/activity/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/fs/monize/ui/activity/dashboard/DashboardActivity.kt
@@ -5,38 +5,32 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.fs.monize.R
+import com.fs.monize.databinding.ActivityDashboardBinding
 import com.fs.monize.ui.activity.signin.SignInActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class DashboardActivity : AppCompatActivity(),
-    BottomNavigationView.OnNavigationItemSelectedListener {
+class DashboardActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navbar)
-        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, TransactionFragment()).commit()
+        binding.navbar.setOnNavigationItemSelectedListener(this)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var frag: Fragment = TransactionFragment()
         when (item.itemId) {
-            R.id.dashboard -> run {
-                val dashboard = Intent(this, DashboardActivity::class.java)
-                startActivity(dashboard)
-            }
-            R.id.Transaksi -> run {
-                val transaksi = Intent(this, SignInActivity::class.java)
-                startActivity(transaksi)
-            }
-            R.id.Simpanan -> {
-                Toast.makeText(this, "Simpanan clicked", Toast.LENGTH_SHORT).show()
-            }
-            R.id.Menabung -> {
-                Toast.makeText(this, "Menabung clicked", Toast.LENGTH_SHORT).show()
-            }
-
+            R.id.dashboard -> Toast.makeText(this, "Dashboard clicked", Toast.LENGTH_SHORT).show()
+            R.id.Transaksi -> frag = TransactionFragment()
+            R.id.Simpanan -> Toast.makeText(this, "Simpanan clicked", Toast.LENGTH_SHORT).show()
+            R.id.Menabung -> Toast.makeText(this, "Menabung clicked", Toast.LENGTH_SHORT).show()
         }
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, frag!!).commit()
         return true
     }
 }
