a3945b547519a24a8befb2b9262d5a265c82c393
Nadya Salsabillah <76547692+Nadyasal@users.noreply.github.com>
2021-09-02 19:19:22 +0700

    Detail and Top Up

---
+++ app/src/main/java/com/fs/monize/ui/activity/transaction/DetilDebtActivity.kt
@@ -0,0 +1,11 @@
+package com.fs.monize.ui.activity.transaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetilDebtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detil_debt)
+    }
+}
