78f66d1efb23662589ed8e589d0c46ad4da7b3b7
ianfyan <imamkurfian@gmail.com>
2021-09-03 09:07:41 +0700

    CRUD Dashboard

--- app/src/main/java/com/fs/monize/ui/activity/transaction/DetilDebtActivity.kt
+++ app/src/main/java/com/fs/monize/ui/activity/transaction/DetilDebtActivity.kt
@@ -1,11 +1,27 @@
 package com.fs.monize.ui.activity.transaction
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.fs.monize.R
+import com.fs.monize.databinding.ActivityDetilDebtBinding
+import com.fs.monize.ui.activity.dashboard.DashboardActivity
 
-class DetilDebtActivity : AppCompatActivity() {
+class DetilDebtActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityDetilDebtBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detil_debt)
+        binding = ActivityDetilDebtBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.deleteTrans.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.dateTrans -> startActivity(Intent(this, DashboardActivity::class.java))
+        }
     }
 }
