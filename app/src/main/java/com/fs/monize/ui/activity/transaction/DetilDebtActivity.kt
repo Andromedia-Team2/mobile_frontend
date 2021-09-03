3eb7ba9413055925006a6be31f42a7032d5d8447
mochammadfawwaz <mailmcfawwaz@gmail.com>
2021-09-03 08:47:12 +0700

    0.4.2 perbaikan build apk

--- app/src/main/java/com/fs/monize/ui/activity/transaction/DetilDebtActivity.kt
+++ app/src/main/java/com/fs/monize/ui/activity/transaction/DetilDebtActivity.kt
@@ -2,6 +2,7 @@ package com.fs.monize.ui.activity.transaction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fs.monize.R
 
 class DetilDebtActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
